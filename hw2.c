11bb21e878727dd442def78c65d5340f8c87c7e3
jht26 <jht2660@gmail.com>
2023-09-22 14:59:01 +0900

    Add C File[hw.2c]

--- hw2.c
+++ hw2.c
@@ -2,17 +2,12 @@
 #include <stdio.h>
 int main(void)
 {
-	int andresult, orresult, xorresult;
-	int num1, num2;
-
-	printf("2개의 정수 입력: ");
-	scanf("%d %d", &num1, &num2); // & \ ^
-
-	andresult = num1 & num2;
-	orresult = num1 | num2;
-	xorresult = num1 ^ num2;
-	printf("%d & %d = %d \n", num1, num2, andresult);
-	printf("%d | %d = %d \n", num1, num2, orresult);
-	printf("%d ^ %d = %d \n", num1, num2, xorresult);
+	float num1; // num1 = km
+	float result; // result = miles
+	printf("please enter kilometers: ");
+	scanf("%f", &num1); 
+	result = num1 * 0.6215040398;
+	printf("%.1f km is equal to %.1f miles \n", num1, result);
 	return 0;
+
 }
