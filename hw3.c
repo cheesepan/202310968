c7cb125f684aa5766f5cac8db710cdaafafdbcd7
jht26 <jht2660@gmail.com>
2023-10-06 02:36:14 +0900

    Add C File [hw3.c]

---
+++ hw3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+	int i, j;
+	int h = 5;
+
+	for (i = 1; i <= h; i++) {
+
+		for (j = 1; j <= h - i; j++) {
+			printf(" ");
+		}
+
+		for (j = 1; j <= 2 * i - 1; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
