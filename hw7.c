4a912bf9e210f834162a867dc72c1ccfa303fce2
cheesepan <86460022+cheesepan@users.noreply.github.com>
2023-11-15 17:40:55 +0900

    Add files via upload

---
+++ hw7.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main()
+{
+	int arr1[6] = { 1, 2, 3, 4, 5, 6 };
+	int arr2[6] = { 7, 8, 9, 10, 11, 12 };
+
+	printf("arr1: ");
+	for (int i = 0; i < 6; ++i)
+	{
+		printf("%d ", arr1[i]);
+	}
+	printf("\n");
+
+	printf("arr2: ");
+	for (int i = 0; i < 6; ++i)
+	{
+		printf("%d ", arr2[i]);
+	}
+	printf("\n");
+
+	for (int i = 0; i < 6; ++i)
+	{
+		int temp = arr1[i];
+		arr1[i] = arr2[i];
+		arr2[i] = temp;
+	}
+	printf("\n");
+
+	printf("After swap:\n");
+	printf("arr1: ");
+	for (int i = 0; i < 6; ++i)
+	{
+		printf("%d ", arr1[i]);
+	}
+	printf("\n");
+
+	printf("arr2: ");
+	for (int i = 0; i < 6; ++i)
+	{
+		printf("%d ", arr2[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
